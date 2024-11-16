c87a49fd8316ea15d21af6ed21c7b4ad456febd2
Carlos Eduardo Miranda <polomiranda@outlook.com>
2024-11-16 11:05:37 -0300

    Change kColorScheme variable to final for improved immutability

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/screens/expenses.dart';
 
-var kColorScheme = ColorScheme.fromSeed(
+final ColorScheme kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
